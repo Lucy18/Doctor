49b7dd980b16a6785fa7d83cb4042cde0d60f0ca
winsky94 <winsky94@163.com>
2017-12-17 16:34:44 +0800

    校验用户信息

--- trunk/Tongue/src/main/java/com/winsky/client/api/UserAPI.java
+++ trunk/Tongue/src/main/java/com/winsky/client/api/UserAPI.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.winsky.APIUtil;
 import com.winsky.bean.UserBean;
 import com.winsky.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,7 +33,21 @@ public class UserAPI {
     @ResponseBody
     public Object add(String json) {
         UserBean userBean = JSONObject.parseObject(json, UserBean.class);
+        String msg = checkUser(userBean);
+        if (msg != null) return APIUtil.genErrorResult(msg);
+
         boolean result = userService.saveOrUpdate(userBean);
         return APIUtil.genBooleanResult(result);
+    }
+
+    private String checkUser(UserBean user) {
+        String msg = null;
+        if (user == null) msg = "用户信息不能为空";
+        else if (StringUtils.isEmpty(user.getOpenId())) msg = "用户编号不能为空";
+        else if (StringUtils.isEmpty(user.getName())) msg = "请在微信公众号内打开";
+        else if (StringUtils.isEmpty(user.getName())) msg = "姓名不能为空";
+        else if (StringUtils.isEmpty(user.getName())) msg = "年龄不能为空";
+        else if (StringUtils.isEmpty(user.getName())) msg = "性别不能为空";
+        return msg;
     }
 }
